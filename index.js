8382649e698bb84613826e032aac5afdbcda2f10
Marco <mcrapts@gmail.com>
2017-10-01 02:51:32 +0200

    improve code

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const util = require('util')
 const hashString = input => crypto.createHash('sha256').update(input).digest('hex')
 
 class Block {
-  constructor(index, transactions, prevHash, nonce) {
+  constructor(index, prevHash, transactions, nonce) {
     this.index = index
-    this.timestamp = Date.now()
-    this.transactions = transactions
     this.prevHash = prevHash
+    this.transactions = transactions
+    this.timestamp = Date.now()
     this.nonce = nonce
+  }
+  get hash() {
+    return hashString(JSON.stringify(this))
   }
 }
 
@@ -29,23 +32,23 @@ class Blockchain {
     this.transactions.push({ from, to, qty })
   }
 
-  hashBlock(block) {
-    return block ? hashString(JSON.stringify(block)) : null
-  }
-
   mineBlock() {
     let nonce = 0
-    let nonceFound
-    while (!nonceFound) {
-      const index = this.chain.length
+    while (true) {
+      const lastBlock = this.lastBlock
+      let index, prevHash
+      if (!lastBlock) {
+        index = 0
+        prevHash = 0
+      } else {
+        index = lastBlock.index + 1
+        prevHash = lastBlock.hash
+      }
       const transactions = JSON.parse(JSON.stringify(this.transactions))
-      const prevHash = this.hashBlock(this.lastBlock)
-      const block = new Block(index, transactions, prevHash, nonce)
-      const hash = this.hashBlock(block)
-      if (hash.startsWith(this.difficulty)) {
+      const block = new Block(index, prevHash, transactions, nonce)
+      if (block.hash.startsWith(this.difficulty)) {
         this.chain.push(block)
         this.transactions = []
-        nonceFound = true
         return block
       } else {
         nonce += 1
