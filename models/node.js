bd10c1d491e7b282b2e0cc6664603b1263ff19a2
Marco <mcrapts@gmail.com>
2017-10-05 21:47:32 +0200

    remove old comments

--- models/node.js
+++ models/node.js
@@ -2,8 +2,5 @@ module.exports = class Node {
   constructor(address) {
     this.address = address
     this.registeredAt = Date.now()
-    // this.lastSync = 0
-    // this.transactions = []
-    // this.chain = []
   }
 }
